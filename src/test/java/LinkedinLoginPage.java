26456864d674dd7f23f3367ea43cc04cc463ae09
marynamostika <marynamostika@gmail.com>
2018-09-03 11:34:16 +0300

    Home task 4 commit

---
+++ src/test/java/LinkedinLoginPage.java
@@ -0,0 +1,56 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static java.lang.Thread.sleep;
+
+public class LinkedinLoginPage {
+
+  private WebDriver driver;
+
+  @FindBy(id = "login-email")
+    private WebElement emailField;
+
+  @FindBy(id = "login-password")
+    private WebElement passwordField;
+
+  @FindBy(id = "login-submit")
+    private WebElement signInButton;
+
+  public LinkedinLoginPage(WebDriver driver) {
+      this.driver = driver;
+      PageFactory.initElements(driver, this);
+  }
+
+  public <T> T login(String userEmail, String userPassword) {
+      emailField.sendKeys(userEmail);
+      passwordField.sendKeys(userPassword);
+      signInButton.click();
+      try {
+          sleep(3000);
+      } catch (InterruptedException e) {
+          e.printStackTrace();
+      }
+      if (getCurrentUrl().contains("/feed")) {
+         return (T) new LinkedinHomePage(driver);
+      }
+      if (getCurrentUrl().contains("/login-submit")) {
+          return (T) new LinkedinLoginSubmitPage(driver);
+      }
+      else {
+          return (T) this;
+      }
+  }
+  public String getCurrentUrl() {
+      return driver.getCurrentUrl();
+  }
+  public String getCurrentTitle() {
+      return driver.getTitle();
+  }
+  public boolean isPageLoaded() {
+      return getCurrentUrl().equals("https://www.linkedin.com/")
+              && getCurrentTitle().equals("LinkedIn: Log In or Sign Up")
+              && signInButton.isDisplayed();
+  }
+}
--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -3,14 +3,37 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static java.lang.Thread.sleep;
 
 public class LinkedinLoginTest {
+    WebDriver driver;
 
-    @Test
-    public void succesfullLoginTest() {
+    @BeforeMethod
+    public void beforeMethod() {
+        driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.quit();
+    }
+
+    @DataProvider
+    public Object[][] validDataProvider() {
+        return new Object[][]{
+                {"marynamostika@ukr.net", "testpassword"},
+                {"marynamostika@ukr.Net", "testpassword"}
+        };
+    }
+
+    @Test(dataProvider = "validDataProvider")
+    public void succesfullLoginTest(String userEmail, String userPassword) {
         //navigate to linkedin.com
         //verify that login page is loaded.
         //enter user email.
@@ -18,31 +41,12 @@ public class LinkedinLoginTest {
         //click on "Sign in" button.
         //verify Home page is displayed.
 
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.linkedin.com/");
+       LinkedinLoginPage linkedinLoginPage = new LinkedinLoginPage(driver);
 
-        WebElement emailField = driver.findElement(By.id("login-email"));
-        WebElement passwordField = driver.findElement(By.id("login-password"));
-        WebElement signInButton = driver.findElement(By.id("login-submit"));
+        Assert.assertTrue(linkedinLoginPage.isPageLoaded(), "Login page is not loaded.");
+        LinkedinHomePage linkedinHomePage = linkedinLoginPage.login(userEmail, userPassword);
 
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/", "Login page Url is wrong");
-        Assert.assertEquals(driver.getTitle(), "LinkedIn: Log In or Sign Up", "Title not matching");
-        Assert.assertTrue(signInButton.isDisplayed(), "signIn button is not displayed in login page.");
-
-        emailField.sendKeys("marynamostika@ukr.net");
-        passwordField.sendKeys("testpassword");
-        signInButton.click();
-
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/feed/", "Home page is not displayed");
-        Assert.assertEquals(driver.getTitle(), "LinkedIn", "Title not matching");
-
-        WebElement profileNavItem = driver.findElement(By.xpath("//li[@id=\"profile-nav-item\"]"));
-        Assert.assertTrue(profileNavItem.isDisplayed(), "profileNavItem button is not displayed on Home page.");
-
-        WebElement dropdownbutton = driver.findElement(By.xpath("//button[@id=\"nav-settings__dropdown-trigger\"]"));
-        dropdownbutton.click();
-        Assert.assertEquals(driver.findElement(By.xpath("//h3[@class='nav-settings__member-name Sans-16px-black-90%-bold']")).getText(),
-                "Testname Testlastname", "My profile name was not found");
+        Assert.assertTrue(linkedinHomePage.isPageLoaded(), "Home page is not loaded.");
     }
 
     @Test
